f83c3739a9401c1257be68d604a8d419d835e0a5
Fernando Salgado <fernando.salgado@modalgr.com.br>
2019-05-23 17:06:25 -0300

    Primeiro Commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+			document.addEventListener('DOMContentLoaded', function() {
+			var elems = document.querySelectorAll('.parallax');
+			var instances = M.Parallax.init(elems);
+		  });
+		    document.addEventListener('DOMContentLoaded', function() {
+			var elems = document.querySelectorAll('.fixed-action-btn');
+			var instances = M.FloatingActionButton.init(elems);
+		  });
+		    $(document).ready(function(){
+			$('.sidenav-cadastro').sidenav({edge: 'right'});
+			$('.sidenav-login').sidenav({edge: 'right'});
+		  });
